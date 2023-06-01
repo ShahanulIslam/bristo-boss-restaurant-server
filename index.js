d3147fb92a75a5ef70e19df12ad135dfe126a012
Your Name <shahanricky86@gmail.com>
2023-06-01 16:45:29 +0600

    carts collection get and post operation

--- index.js
+++ index.js
@@ -32,16 +32,37 @@ async function run() {
 
         const menuCollection = client.db("BistroDB").collection("menu");
         const reviewsCollection = client.db("BistroDB").collection("reviews");
+        const cartsCollection = client.db("BistroDB").collection("carts");
 
         app.get("/menu", async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result)
         })
 
+
         app.get("/review", async (req, res) => {
             const result = await reviewsCollection.find().toArray();
             res.send(result)
         })
+
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            console.log(email)
+            if (!email) {
+                res.send([])
+            }
+            const query = { email: email }
+            const result = await cartsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.post("/carts", async (req, res) => {
+            const item = req.body;
+            const result = await cartsCollection.insertOne(item);
+            res.send(result)
+        })
+
+
 
 
         // Send a ping to confirm a successful connection
